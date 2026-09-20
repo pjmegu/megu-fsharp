f6c24e9c31b666489cc45107d2ae1b191ee48bc5
sakura aoi <107183580+0x5341@users.noreply.github.com>
2026-09-20 13:11:26 +0000

    add exec cmd

--- Megu.App/Program.fs
+++ Megu.App/Program.fs
@@ -1,2 +1,39 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.CommandLine
+open System.IO
+
+let exec script =
+    printfn "Executing script: %s" script
+
+[<EntryPoint>]
+let main args =
+    let rootCommand = RootCommand("Megu Compiler")
+
+    let inputPath =
+        Option<string>("--input", [| "-i" |])
+        |> fun o ->
+            o.Description <- "Input file path"
+            o
+    rootCommand.Options.Add(inputPath)
+
+    let script =
+        Option<string>("--script", [| "-s" |])
+        |> fun o ->
+            o.Description <- "Script"
+            o
+    rootCommand.Options.Add(script)
+
+    rootCommand.SetAction(fun result ->
+        let inputPath = result.GetValue(inputPath)
+        let script = result.GetValue(script)
+
+        match inputPath, script with
+        | i, null ->
+            let script = File.ReadAllText(i)
+            exec script
+        | null, s ->
+            exec s
+        | null, null | _, _ ->
+            printfn "Error: Please provide only one of input file path or script."
+    )
+
+    rootCommand.Parse(args).Invoke()
