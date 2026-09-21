d7af4b73a9eb6b6a6562b9efdc358d315cceb6a1
sakura aoi <107183580+0x5341@users.noreply.github.com>
2026-09-21 22:54:19 +0000

    add tmpl impl

--- Megu.Compiler/Parser.fs
+++ Megu.Compiler/Parser.fs
@@ -2,3 +2,5 @@ module Megu.Compiler.Parser
 
 open Megu.Compiler.Token
 open Megu.Compiler.AST
+
+let parse tokens: Node = raise (System.NotImplementedException("Parser.parse is not implemented yet."))
